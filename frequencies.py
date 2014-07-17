3dee9851b5de474373a6bc31f900d31b5027ee40
Stephen <mstephenmsmith@gmail.com>
2014-07-16 17:20:31 -0700

    updates

---
+++ frequencies.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import numpy as np
+import sys, getopt
+
+def get_means_medians(df, user_id):
+
+	temp_indices = np.where(df['user_id'] == user_id)
+	temp_indices = list(temp_indices[0])
+
+	temp_dates = list(df['created_on'].iloc[temp_indices])
+
+	temp_dates = sorted(temp_dates)
+
+	diffs = []
+
+	for j in xrange(len(temp_dates)-1):
+		temp_diff = temp_dates[j+1]-temp_dates[j]
+		diffs.append(temp_diff)
+	
+	diffs = [x.days for x in diffs]
+
+	if np.mean(diffs) != 0:
+		std_temp = float(np.std(diffs))/float(np.mean(diffs))
+	else:
+		std_temp = -1
+	
+	mean_temp = np.mean(diffs)
+	median_temp = np.median(diffs)
+
+	return user_id, mean_temp, median_temp, std_temp
+
+def main(argv):
+
+	inputfile = ''
+	outputfile = ''
+	try:
+		opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+	except getopt.GetoptError:
+		print 'test.py -i <inputfile> -o <outputfile>'
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print 'test.py -i <inputfile> -o <outputfile>'
+			sys.exit()
+		elif opt in ("-i", "--ifile"):
+			inputfile = arg
+		elif opt in ("-o", "--ofile"):
+			outputfile = arg
+	print 'Input file is "', inputfile
+	print 'Output file is "', outputfile
+
+
+	df = pd.read_csv(inputfile)
+
+	# df = df.iloc[:5000,:]
+
+	df['created_on'] = pd.to_datetime(df['created_on']);
+
+	print "Getting max and min creation dates..."
+
+	max_created_on = df[['user_id','created_on']].groupby('user_id').max().reset_index()[['user_id','created_on']]
+	max_created_on.columns = ['user_id','last_purch_date']
+
+	min_created_on = df[['user_id','created_on']].groupby('user_id').min().reset_index()[['user_id','created_on']]
+	min_created_on.columns = ['user_id','first_purch_date']
+
+	ns_in_day = float(8.64*10**13)
+
+	all_stds = []
+	all_means = []
+	all_medians = []
+	all_user_ids = []
+
+	unique_user_ids = list(set(df['user_id']))
+
+	print "Getting medians and means..."
+
+	for i, user_id in enumerate(unique_user_ids):
+		if i%10000 ==0:
+			print i
+		user_id, mean_temp, median_temp, std_temp = get_means_medians(df, user_id)
+
+		all_stds.append(std_temp)
+		all_means.append(mean_temp)
+		all_medians.append(median_temp)
+		all_user_ids.append(user_id)
+
+	d_ = {'user_id': all_user_ids, 'mean_freq': all_means, 'median_freq': all_medians, 'std_freq': all_stds}
+
+	output_df = pd.DataFrame(data=d_)
+
+	output_df = pd.merge(output_df, min_created_on, on = ['user_id'])
+
+	output_df = pd.merge(output_df, max_created_on, on = ['user_id'])
+
+	output_df.to_csv(outputfile)
+
+if __name__ == '__main__':
+	main(sys.argv[1:])
--- survival_analysis.py
+++ survival_analysis.py
@@ -2,60 +2,29 @@ import pandas as pd
 import numpy as np
 import sys, getopt
 import matplotlib.pyplot as plt
-from lifelines.estimation import KaplanMeierFitter as kmf
+from lifelines.estimation import KaplanMeierFitter
 
-def get_churn_data(df, user_col, date_col, min_date, max_date, time_to_churn = 60):
+def get_churn_data(df, min_date, max_date, time_to_churn = 1):
 	
 	ns_in_day = float(8.64*10**13)
 	
-	max_date_overall = df[date_col].max()
+	max_date_overall = df[max_date].max()
 	
-	diff_from_max_date = max_date[date_col].apply(lambda x: max_date_overall - x)
+	diff_from_max_date = df[max_date].apply(lambda x: max_date_overall - x)
 
 	churn = np.where(diff_from_max_date.apply(lambda x: x.item()/ns_in_day) > time_to_churn,1,0)
 	
-	max_date[date_col].iloc[np.where(diff_from_max_date.apply(lambda x: x.item()/ns_in_day) < time_to_churn)[0]] = max_date_overall
+	last_date_of_interaction = df[max_date]
+
+	last_date_of_interaction.iloc[np.where(diff_from_max_date.apply(lambda x: x.item()/ns_in_day) < time_to_churn)[0]] = max_date_overall
 	
-	length_of_life = (max_date[date_col] - min_date[date_col]).apply(lambda x: x.item()/ns_in_day)
+	length_of_life = (last_date_of_interaction - df[min_date]).apply(lambda x: x.item()/ns_in_day)
 	
-	df = pd.DataFrame(list(min_date[date_col]), columns=['min_date'])
-	df['max_date'] = list(max_date[date_col])
 	df['duration'] = list(length_of_life)
 	df['churn'] = list(churn)
 	
 	return df
 
-# Calculate the mean, median, and standard deviation of the difference
-# in sequential time periods for each user
-
-def get_means_medians(df, user_id):
-
-	temp_indices = np.where(df['user_id'] == user_id)
-	temp_indices = list(temp_indices[0])
-
-	temp_dates = list(df['created_on'].iloc[temp_indices])
-
-	temp_dates = sorted(temp_dates)
-
-	# temp_dates = [l]
-
-	diffs = []
-
-	for j in xrange(len(temp_dates)-1):
-		temp_diff = temp_dates[j+1]-temp_dates[j]
-		diffs.append(temp_diff)
-	
-	diffs = [x.days for x in diffs]
-
-	if np.mean(diffs) != 0:
-		std_temp = float(np.std(diffs))/float(np.mean(diffs))
-	else:
-		std_temp = -1
-	
-	mean_temp = np.mean(diffs)
-	median_temp = np.median(diffs)
-
-	return mean_temp, median_temp, std_temp
 
 def main(argv):
 
@@ -79,51 +48,124 @@ def main(argv):
 
 	df = pd.read_csv(inputfile)
 
-	df['created_on'] = pd.to_datetime(df['created_on']);
-
-	print "Getting max and min creation dates..."
-
-	max_created_on = df[['user_id','created_on']].groupby('user_id').max()
-
-	min_created_on = df[['user_id','created_on']].groupby('user_id').min()
-
-	ns_in_day = float(8.64*10**13)
-
-	all_stds = []
-	all_means = []
-	all_medians = []
-
-	unique_user_ids = list(set(df['user_id']))
-
-	print len(unique_user_ids)
-
-	print "Getting medians and means..."
-
-	for i, user_id in enumerate(unique_user_ids):
-		if i%1000 ==0:
-			print i
-		mean_temp, median_temp, std_temp = get_means_medians(df, user_id)
-
-		all_stds.append(std_temp)
-		all_means.append(mean_temp)
-		all_medians.append(median_temp)
+	df['first_purch_date'] = pd.to_datetime(df['first_purch_date'])
+	df['last_purch_date'] = pd.to_datetime(df['last_purch_date'])
 
 	print "Getting churn data...."
 
-	all_churn_data = get_churn_data(df,'user_id','created_on',min_created_on, max_created_on)
+	all_churn_data = get_churn_data(df, "first_purch_date", "last_purch_date")
 
-	all_churn_data['means'] = all_means
-	all_churn_data['medians'] = all_medians
-	all_churn_data['stds'] = all_stds
+	all_churn_data_gt0 = all_churn_data[(all_churn_data.duration > 0) & (all_churn_data.std_freq >=0)]
 
-	all_churn_data_gt0 = all_churn_data[all_churn_data.duration > 0]
+	buckets = [0,7,14,30,90,400]
+	all_churn_data_gt0["freq_bucket"] = all_churn_data_gt0["mean_freq"]
+	all_churn_data_gt0.freq_bucket = pd.cut(all_churn_data_gt0.freq_bucket, buckets)
 
-	T = all_churn_data_gt0['duration']
-	C = all_churn_data_gt0['churn']
+	unique_buckets = set(all_churn_data_gt0.freq_bucket.order())
 
-	kmf.fit(T, event_observed = C)
+	kmf_buckets = []
+	for bucket in unique_buckets:
+		indices_ = np.where(all_churn_data_gt0.freq_bucket == bucket)
+		T = all_churn_data_gt0['duration'].iloc[indices_]
+		C = all_churn_data_gt0['churn'].iloc[indices_]
+	
+	kmf = KaplanMeierFitter()
 
-	print kmf.median_
+	for jj, kmf_ in enumerate(kmf_buckets):
+		print kmf_, kmf_.median_
+		if jj==0:
+			ax = kmf_.plot()
+		else:
+			kmf_.plot(ax=ax)
+
+# plt.savefig("survival_rates.png")
+
+def plot_(filename):
+
+	df = pd.read_csv(filename)
+	print df.shape
+	df = df.dropna()
+	print df.shape
+
+	df['first_purch_date'] = pd.to_datetime(df['first_purch_date'])
+	df['last_purch_date'] = pd.to_datetime(df['last_purch_date'])
+
+	print "Getting churn data...."
+
+	all_churn_data = get_churn_data(df, "first_purch_date", "last_purch_date")
+
+	all_churn_data_gt0 = all_churn_data[(all_churn_data.duration > 0) & (all_churn_data.std_freq >=0) & (all_churn_data.mean_freq > 0)]
+
+	buckets = [0,7,14,30,90,400]
+	all_churn_data_gt0["freq_bucket"] = all_churn_data_gt0["mean_freq"]
+	all_churn_data_gt0.freq_bucket = pd.cut(all_churn_data_gt0.freq_bucket, buckets)
+
+	unique_buckets = list(set(all_churn_data_gt0.freq_bucket.order()))[1:]
+
+	print unique_buckets
+
+	# print all_churn_data_gt0.iloc[np.where(all_churn_data_gt0.freq_bucket == unique_buckets[1])[0]]
+
+
+	kmf_buckets = []
+	for bucket in unique_buckets:
+		indices_ = np.where(all_churn_data_gt0.freq_bucket == bucket)
+		T = all_churn_data_gt0['duration'].iloc[indices_]
+		C = all_churn_data_gt0['churn'].iloc[indices_]
+		kmf = KaplanMeierFitter()
+		kmf.fit(T, event_observed = C, label=bucket)
+		kmf_buckets.append(kmf)
+
+	print len(kmf_buckets)
+
+	for jj, kmf_ in enumerate(kmf_buckets):
+		print kmf_, kmf_.median_
+		if jj==0:
+			ax = kmf_.plot()
+		else:
+			kmf_.plot(ax=ax)
 
 if __name__ == '__main__':
-	main(sys.argv[1:])
+ 	main(sys.argv[1:])
+ 	df = pd.read_csv(filename)
+	print df.shape
+	df = df.dropna()
+	print df.shape
+
+	df['first_purch_date'] = pd.to_datetime(df['first_purch_date'])
+	df['last_purch_date'] = pd.to_datetime(df['last_purch_date'])
+
+	print "Getting churn data...."
+
+	all_churn_data = get_churn_data(df, "first_purch_date", "last_purch_date")
+
+	all_churn_data_gt0 = all_churn_data[(all_churn_data.duration > 0) & (all_churn_data.std_freq >=0) & (all_churn_data.mean_freq > 0)]
+
+	buckets = [0,7,14,30,90,400]
+	all_churn_data_gt0["freq_bucket"] = all_churn_data_gt0["mean_freq"]
+	all_churn_data_gt0.freq_bucket = pd.cut(all_churn_data_gt0.freq_bucket, buckets)
+
+	unique_buckets = list(set(all_churn_data_gt0.freq_bucket.order()))[1:]
+
+	print unique_buckets
+
+	# print all_churn_data_gt0.iloc[np.where(all_churn_data_gt0.freq_bucket == unique_buckets[1])[0]]
+
+
+	kmf_buckets = []
+	for bucket in unique_buckets:
+		indices_ = np.where(all_churn_data_gt0.freq_bucket == bucket)
+		T = all_churn_data_gt0['duration'].iloc[indices_]
+		C = all_churn_data_gt0['churn'].iloc[indices_]
+		kmf = KaplanMeierFitter()
+		kmf.fit(T, event_observed = C, label=bucket)
+		kmf_buckets.append(kmf)
+
+	print len(kmf_buckets)
+
+	for jj, kmf_ in enumerate(kmf_buckets):
+		print kmf_, kmf_.median_
+		if jj==0:
+			ax = kmf_.plot()
+		else:
+			kmf_.plot(ax=ax)
